1ee3508ff6e908cd575529ba1b466f5d62b6143f
Manuel Favela <mfavela@mit.edu>
2021-04-01 00:39:42 -0400

    title scene lol

--- AmongThem/Assets/Scripts/LevelLoader.cs
+++ AmongThem/Assets/Scripts/LevelLoader.cs
@@ -24,8 +24,43 @@ public class LevelLoader : MonoBehaviour
         //add button to trigger pokemon cut sequence on every counter (this is unique to other build)
     }
 
+
     public void LoadNextLevel(){
+    	//GameObject image = transform.GetChild(0).gameObject.transform.GetChild(1).gameObject; 
+    	//image.GetComponent<CanvasGroup>().alpha = 1f; 
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex +1));
+
+    }
+
+    public void PlayGame(){
+    	GameObject background = transform.GetChild(0).gameObject.transform.GetChild(1).gameObject; 
+    	background.SetActive(false); 
+
+    	LoadNextLevel(); 
+
+    }
+
+    public void LoadCredit(){
+    	GameObject creditScene = transform.GetChild(0).gameObject.transform.GetChild(5).gameObject; 
+    	GameObject backButton = transform.GetChild(0).gameObject.transform.GetChild(7).gameObject; 
+    	creditScene.SetActive(true); 
+    	backButton.SetActive(true); 
+    }
+
+    public void LoadHowTo(){
+    	GameObject howToScene = transform.GetChild(0).gameObject.transform.GetChild(6).gameObject; 
+    	GameObject backButton = transform.GetChild(0).gameObject.transform.GetChild(7).gameObject; 
+    	howToScene.SetActive(true); 
+    	backButton.SetActive(true); 
+    }
+
+    public void backTitle(){
+    	GameObject backButton = transform.GetChild(0).gameObject.transform.GetChild(7).gameObject; 
+    	GameObject creditScene = transform.GetChild(0).gameObject.transform.GetChild(5).gameObject; 
+    	GameObject howToScene = transform.GetChild(0).gameObject.transform.GetChild(6).gameObject; 
+    	creditScene.SetActive(false); 
+    	howToScene.SetActive(false); 
+    	backButton.SetActive(false); 
     }
 
     public void RestartGame(){
